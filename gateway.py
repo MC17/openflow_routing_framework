0994e2420157d1d5b82aa1ac478c4deb8098c7a9
Can Zhang <can@canx.me>
2013-04-12 16:40:09 +0800

    add ipv6prefixlen, fix PEP8

--- gateway.py
+++ gateway.py
@@ -1,14 +1,19 @@
 
 
 class Gateway(object):
-    def __init__(self, name='', ip='', ipv6='', num = '', prefixlen='', mac=''):
+    def __init__(
+            self, name='', ip='', ipv6='', num='',
+            prefixlen='', ipv6prefixlen='', mac=''):
         self.port_name = name
         self.gw_ip = ip
         self.gw_ipv6 = ipv6
         self.port_num = num
-        self.prefixlen = prefixlen  
+        self.prefixlen = prefixlen
+        self.ipv6prefixlen = ipv6prefixlen
         self.mac = mac
 
     def __str__(self):
-        return 'Gateway<name=%s,gw_ip=%s,gw_ipv6=%s,port_num=%s,prefixLen=%s,mac=%s>' % \
-        (self.port_name, self.gw_ip, self.gw_ipv6, self.port_num, self.prefixlen, self.mac)
+        return 'Gateway<name=%s,gw_ip=%s,gw_ipv6=%s,port_num=%s,\
+prefixLen=%s,ipv6prefixlen=%s,mac=%s>' % (
+            self.port_name, self.gw_ip, self.gw_ipv6, self.port_num,
+            self.prefixlen, self.ipv6prefixlen, self.mac)
