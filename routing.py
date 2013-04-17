fe976ef6761b58e5f6a0403775e61f60f3215868
Can Zhang <can@canx.me>
2013-04-17 22:21:46 +0800

    unfinished

--- routing.py
+++ routing.py
@@ -1,4 +1,6 @@
 import gevent
+import time
+
 
 from ryu.base import app_manager
 from ryu.controller.handler import set_ev_handler, set_ev_cls
@@ -6,22 +8,32 @@ from ryu.controller.handler import (HANDSHAKE_DISPATCHER, MAIN_DISPATCHER,
                                     CONFIG_DISPATCHER, DEAD_DISPATCHER)
 from ryu.controller import ofp_event
 from ryu import topology
-from ryu.ofproto import ofproto_v1_0
+from ryu.ofproto import ofproto_v1_0, nx_match
+from ryu.ofproto import ether, inet
 from ryu.lib.packet import (packet, ethernet, arp, icmp, icmpv6, ipv4, ipv6,
                             tcp, udp)
-from ryu.ofproto import ether, inet
 
 from switch import Port, Switch
-
 from util import read_cfg
+import algorithm
+import convert
 
 class Routing(app_manager.RyuApp):
+    ARP_TIMEOUT = 600    # in seconds
+
+    FLOW_IDLE_TIMEOUT = 60
+    FLOW_HARD_TIMEOUT = 600
 
     def __init__(self, *args, **kwargs):
         super(Routing, self).__init__(*args, **kwargs)
         
         self.dpid_to_switch = {}    # dpid_to_switch[dpid] = Switch
                                     # maintains all the switches
+        self.ip_to_mac = {}     # ip_to_mac[ip_addr] = (mac_addr, 
+                                #                       time_stamp)
+                                # maintains ARP table
+        self.routing_algo = algorithm.Dijkstra(self.dpid_to_switch)
+
         self.filepath = 'config.xml'
 
         try:
@@ -136,8 +148,16 @@ class Routing(app_manager.RyuApp):
             if port_no not in switch.ports:
                 p = Port(port = port, dp = event.msg.datapath)
                 switch.ports[p.port_no] = p
+
             if port_no == ofproto_v1_0.OFPP_LOCAL:
                 switch.name = port.name.rstrip('\x00')
+            else:
+                # port.curr is a number of 32 bits, only used 12 bits in ovs
+                # represents current features of the port.
+                # LOCAL port doesn't have
+                curr = port.curr & 0x7f	# get last 7 bits
+                p.cost = 64/curr
+                print p.cost
 
         switch.update_from_config(self.switch_cfg)
 
@@ -197,11 +217,12 @@ class Routing(app_manager.RyuApp):
     def _handle_icmp(self, msg, pkt, icmp_pkt):
         '''
             reply to ICMP_ECHO_REQUEST(i.e. ping);
-            may handle other types of ICMP msg in the future
+            may handle other types of ICMP msg in the future;
+            return True if send a responce
         '''
         print 'icmp', icmp_pkt
         if icmp_pkt.type != icmp.ICMP_ECHO_REQUEST:
-            return
+            return False
 
         in_port_no = msg.in_port
         switch = self.dpid_to_switch[msg.datapath.id]
@@ -215,7 +236,7 @@ class Routing(app_manager.RyuApp):
                 need_reply = True
                 break
         if not need_reply:
-            return
+            return False
         
         echo_id = icmp_pkt.data.id
         echo_seq = icmp_pkt.data.seq
@@ -243,6 +264,7 @@ class Routing(app_manager.RyuApp):
                 actions=[datapath.ofproto_parser.OFPActionOutput(in_port_no)],
                 data=p.data)
         print 'send a ping replay'
+        return True
 
     def _handle_icmpv6(self, msg, pkt, icmpv6_pkt):
         print 'icmpv6', icmpv6_pkt
@@ -252,7 +274,7 @@ class Routing(app_manager.RyuApp):
         if icmpv6_pkt.type_ == icmpv6.ND_NEIGHBOR_SOLICIT:
             port = switch.ports[in_port_no]
             if port.gateway and icmpv6_pkt.data.dst != port.gateway.gw_ipv6:
-                return
+                return False
             #send a ND_NEIGHBOR_REPLY packet
             ether_layer = self.find_packet(pkt, 'ethernet')
             ether_dst = ether_layer.src
@@ -314,12 +336,160 @@ class Routing(app_manager.RyuApp):
                     data=p.data)
             print 'send a ping6 reply packet'
 
+        return True
+
+    def _remember_mac_addr(self, packet, _4or6):
+        '''
+            get ip <-> mac relationship from packets and 
+            store them in dict ip_to_mac
+        '''
+        time_now = time.time()
+        ether_layer = self.find_packet(packet, 'ethernet')
+        if _4or6 == 4:
+            ip_layer = self.find_packet(packet, 'ipv4')
+        else:
+            ip_layer = self.find_packet(packet, 'ipv6')
+        self.ip_to_mac[ip_layer.src] = (ether_layer.src, time_now)
+        
+
+    def deploy_flow_entry(self, datapath, instruction, _4or6):
+        '''
+            deploy flow entry into switch
+        '''
+        # XXX
+        pass
+
+    def last_switch_out(self, msg, outport_no, _4or6):
+        if _4or6 == 4:
+            ip_layer = self.find_packet('ipv4')
+        else:
+            ip_layer = self.find_packet('ipv6')
+
+        try:
+            mac_addr = self.ip_to_mac[ip_layer.dst]
+        except KeyError:
+            if _4or6 == 4:
+                self._send_arp_request(msg.datapath, outport_no,
+                                   ip_layer.dst)
+            else:
+                self._send_icmp_NS(msg.datapath, outport_no,
+                                    ip_layer.dst)
+            return
+
+        dp = msg.datapath
+        switch = self.dpid_to_switch[dp.id]
+
+        if _4or6 == 4:
+            # ip src exact match
+            wildcards = ofproto_v1_0.OFPFW_ALL
+            wildcards &= ~ofproto_v1_0.OFPFW_DL_TYPE
+            wildcards &= ~(0x3f << ofproto_v1_0.OFPFW_NW_DST_SHIFT)
+
+            match = dp.ofproto_parser.OFPMatch(
+                    # because of wildcards, parameters other than dl_type
+                    # and nw_dst could be any value
+                    wildcards = wildcards, in_port = 0, 
+                    dl_src = 0, dl_dst = 0, dl_vlan = 0, dl_vlan_pcp = 0,
+                    dl_type = ether.ETH_TYPE_IP, nw_tos = 0, nw_proto = 0,
+                    nw_src = 0, nw_dst = ip_layer.dst, tp_src = 0,
+                    tp_dst = 0)
+        else:
+            rule = nx_match.ClsRule()
+            rule.set_dl_type(ether.ETH_TYPE_IPV6)
+            rule.set_ipv6_dst(ip_layer.dst)
+
+        actions = []
+        actions.append(dp.ofproto_parser.OFPActionSetDlSrc(
+                        switch.ports[outport_no].hw_addr))
+        actions.append(dp.ofproto_parser.OFPActionSetDlDst(
+                        mac_addr))
+        actions.append(dp.ofproto_parser.OFPActionOutput(outport_no))
+        
+        if _4or6 == 4:
+            mod = dp.ofproto_parser.OFPFlowMod(
+                    datapath = dp, match = match, cookie = 0,
+                    command = dp.ofproto.OFPFC_MODIFY,
+                    idle_timeout = Routing.FLOW_IDLE_TIMEOUT,
+                    hard_timeout = Routing.FLOW_HARD_TIMEOUT,
+                    out_port = outport_no, actions = actions)
+        else:
+            mod = dp.ofproto_parser.NXTFlowMod(
+                    datapath = dp, cookie = 0, 
+                    command = dp.ofproto.OFPFC_MODIFY,
+                    idle_timeout = Routing.FLOW_IDLE_TIMEOUT,
+                    hard_timeout = Routing.FLOW_HARD_TIMEOUT,
+                    out_port = outport_no, rule = rule,
+                    actions = actions)
+ 
+        out = dp.ofproto_parser.OFPPacketOut(
+            datapath = dp, buffer_id = msg.buffer_id,
+            in_port = msg.in_port, actions = actions,
+            data = msg.data)
+
+        dp.send_msg(mod)
+        dp.send_msg(out)
+            
+
+    def find_switch_of_network(self, dst_addr, _4or6):
+        for dpid, switch in self.dpid_to_switch.iteritems():
+            for port_no, port in switch.ports.iteritems():
+                if _4or6 == 4:
+                    if port.gateway and convert.ipv4_in_network(dst_addr,
+                                    port.gateway.gw_ip, 
+                                    port.gateway.prefixlen):
+                        return self.dpid_to_switch[dpid], port_no
+                else:
+                    if port.gateway and convert.ipv6_in_network(dst_addr,
+                                    port.gateway.gw_ipv6,
+                                    port.gateway.ipv6prefixlen):
+                        return self.dpid_to_switch[dpid], port_no
+        return None, None
 
     def _handle_ipv4(self, msg, pkt, protocol_pkt):
         print 'ipv4', protocol_pkt
+        self._remember_mac_addr(pkt, 4)
+
+        try:
+            icmp_layer = self.find_packet(pkt, 'icmp')
+            if self._handle_icmp(msg, pkt, icmp_layer):
+                # if icmp method handles this packet successfully,
+                # further processing is not needed
+                return
+        except:
+            pass
+        
+        src_switch = self.dpid_to_switch[msg.datapath.id]
+        dst_switch, dst_port_no = self.find_switch_of_network(
+                                        protocol_pkt.dst, 4)
+        if src_switch == dst_switch:
+            self.last_switch_out(msg, dst_port_no, 4)
+            return
+        elif dst_switch == None:
+            # can't find destination in this domain
+            # raise an event to `moudle B`
+            # XXX
+            pass
+
+        result = self.routing_algo.find_route(src_switch, dst_switch)
+        if result:
+            self.deploy_flow_entry(msg.datapath, result, 4)
+        else:
+            self.drop_pkt(msg)
+        
 
     def _handle_ipv6(self, msg, pkt, protocol_pkt):
         print 'ipv6', protocol_pkt
+        self._remember_mac_addr(pkt, 6)
+
+        try:
+            icmpv6_layer = self.find_packet(pkt, 'icmpv6')
+            if self._handle_icmpv6(msg, pkt, icmpv6_layer):
+                return
+        except:
+            pass
+
+        result = self.routing_algo(msg, pkt, 6)
+        self.deploy_flow_entry(msg.datapath, result, 6)
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def packet_in_handler(self, event):
@@ -328,16 +498,11 @@ class Routing(app_manager.RyuApp):
         for p in pkt.protocols:
             if isinstance(p, arp.arp):
                 self._handle_arp(event.msg, pkt, p)
-            elif isinstance(p, icmp.icmp):
-                self._handle_icmp(event.msg, pkt, p)
-            elif isinstance(p, icmpv6.icmpv6):
-                self._handle_icmpv6(event.msg, pkt, p)
-            # ipv4 and ipv6 only handle "normal" packets,
-            # i.e. packets that not send to/should be received by controller
+            # ipv4 and ipv6 also handle their corresponding icmp packets
             elif isinstance(p, ipv4.ipv4):
                 self._handle_ipv4(event.msg, pkt, p)
             elif isinstance(p, ipv6.ipv6):
                 self._handle_ipv6(event.msg, pkt, p)
             else:
-                # should be more classifications here, BGP/OSPF etc.
+                # might be more classifications here, BGP/OSPF etc.
                 pass
