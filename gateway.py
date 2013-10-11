d6fb2a48137961717b6c7b43be982a784d73aa24
Can Zhang <can@canx.me>
2013-10-11 22:29:27 +0800

    refine gateway module with netaddr

--- gateway.py
+++ gateway.py
@@ -1,21 +1,20 @@
-import convert
-
+import netaddr
 
 class Gateway(object):
     def __init__(
-            self, name='', ip='', ipv6='', num='',
+            self, name='', ip='', ipv6='', port_no='',
             prefixlen='', ipv6prefixlen='', border=False):
         self.port_name = name
-        self.gw_ip = convert.ipv4_to_int(ip)
-        self.gw_ipv6 = convert.ipv6_to_bin(ipv6)
-        self.port_num = num
-        self.prefixlen = int(prefixlen)
-        self.ipv6prefixlen = int(ipv6prefixlen)
-        self.border = border
+        self.gw_ip = netaddr.IPNetwork(ip)
+        self.gw_ip.prefixlen = int(prefixlen)
+        self.gw_ipv6 = netaddr.IPNetwork(ipv6)
+        self.gw_ipv6.prefixlen = int(ipv6prefixlen)
+        self.port_no = port_no
+        self.isBorder = border
 
     def __str__(self):
-        return 'Gateway<name=%s,gw_ip=%s,gw_ipv6=%s,port_num=%s,\
-prefixLen=%s,ipv6prefixlen=%s,border=%s>' % (
-            self.port_name, convert.ipv4_to_str(self.gw_ip),
-            convert.bin_to_ipv6(self.gw_ipv6),
-            self.port_num, self.prefixlen, self.ipv6prefixlen, self.border)
+        return 'Gateway<name=%s, gw_ip=%s, gw_ipv6=%s, port_num=%s, border=%s>'\
+            % (
+                self.port_name, str(self.gw_ip), str(self.gw_ipv6),
+                self.port_no, self.isBorder
+              )
